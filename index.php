b4a59a847a02ad6e5dab5dd7eb0d4c43815e3405
David Kovalevich <kovalevichdavid@yahoo.com>
2019-02-03 15:14:26 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Name: David Kovalevich
+ * Date: 01/19/2019
+ * Fat Free Framework
+ */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload
+require_once ('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Turn of Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function() {
+    $view = new View;
+    echo $view->render('views/home.html');
+});
+
+//Run fat free
+$f3->run();
